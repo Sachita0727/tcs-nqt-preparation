2897ad5482c9209f60e189af5358faec2bcc2f0b
Sachita Seth <22BCS12150@cuchd.in>
2026-04-02 02:53:06 +0530

    Add palindrome check for user input number

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,13 @@
+n = int(input("Enter the number: "))
+rev = 0
+dummy = n
 
+while n > 0:
+    temp = n % 10
+    rev = rev * 10 + temp
+    n = n // 10
+
+if dummy == rev:
+    print("It is a palindrome")
+else:
+    print("It is not a palindrome")
