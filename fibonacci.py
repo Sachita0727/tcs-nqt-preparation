3209ac219c4aca0280e1f254df42f02dbdece5e2
Sachita Seth <22BCS12150@cuchd.in>
2026-04-02 02:51:56 +0530

    Add Fibonacci series calculation
    Implement Fibonacci series calculation up to N terms.

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,16 @@
+n = int(input("Enter the value of N: "))
 
+if n == 0:
+    print("The Fibonacci Series up to", n, "th term:")
+    print(0)
+else:
+    second_last = 0
+    last = 1
+    print("The Fibonacci Series up to", n, "th term:")
+    print(second_last, last, end=" ")
+
+    for i in range(2, n + 1):
+        cur = last + second_last
+        second_last = last
+        last = cur
+        print(cur, end=" ")
